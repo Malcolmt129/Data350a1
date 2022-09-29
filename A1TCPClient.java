fefb121fe876ce411f2571fe0f0b72d30875cc1d
Malcolmt129 <malcolm.t129@gmail.com>
2022-09-29 18:12:32 -0400

    working on client

--- A1TCPClient.java
+++ A1TCPClient.java
@@ -7,13 +7,19 @@ public class A1TCPClient {
         
         System.out.println("Enter an integer 1, 2, or 3: ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //This basically like creating scanner class 
-        int choice = br.read();
-        br.close();
+        String choice = br.readLine();
+        int number = Integer.parseInt(choice);
 
-        System.out.println(choice);
+        Socket clientSocket = new Socket("localhost", 11111);
+
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream();
+
+        BufferedReader infromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        
 
         //Send the integer to the server...
-        
+
 
 
 
