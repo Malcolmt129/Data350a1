3fc2a5d55a042e4fc91671f7e2c529e5005f060d
root <root@DESKTOP-U6QSL69.localdomain>
2022-10-08 12:26:40 -0400

    fully functional

--- A1TCPClient.java
+++ A1TCPClient.java
@@ -4,9 +4,11 @@ import java.net.*;
 
 public class A1TCPClient {
     public static void main(String[] args) throws Exception {
+        String sentence; 
+        System.out.println();
         
         System.out.println("Enter an integer 1, 2, or 3: ");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //This basically like creating scanner class 
+        BufferedReader infromUser = new BufferedReader(new InputStreamReader(System.in)); //This basically like creating scanner class 
 
         Socket clientSocket = new Socket("localhost", 11111);
 
@@ -14,10 +16,16 @@ public class A1TCPClient {
 
         BufferedReader infromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        
-
+        sentence =infromUser.readLine();
         //Send the integer to the server...
-        outToServer.writeBytes(br.readLine());
+        outToServer.writeBytes(sentence + "\n");
+
+        //Message from server
+
+        do {
+            System.out.println(infromServer.readLine());
+        } while (infromServer.readLine() != null);
         
+        clientSocket.close();
     }
 }
--- A1TCPServer.java
+++ A1TCPServer.java
@@ -1,66 +1,60 @@
 import java.io.*;
 import java.net.*;
-import java.time.*;
-import java.util.concurrent.TimeUnit;
 
 public class A1TCPServer {
-    
-    public static URL getPage(URL url) throws IOException, InterruptedException{
-        URL site = url;
-        HttpURLConnection con = (HttpURLConnection) site.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-        System.out.println("Request sent to " + site + "...");
-
-        TimeUnit.SECONDS.sleep(3);
         
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
-
-        return site;
-    }
-    
-    
-    
-    
     public static void main(String[] args) throws IOException, InterruptedException {
     
         String clientString;
         ServerSocket welcomeSocket = new ServerSocket(11111);
+        String confirmation = "Content received from ";
+        String w = "";
+        Socket connSocket = new Socket();
 
         while (true) {
-            Socket connSocket = welcomeSocket.accept();
+            
+            try {
+                connSocket = welcomeSocket.accept();
+            } catch (Exception e) {
+                System.out.println("Socket is closed now!");
+            }
+            System.out.println("Client connected");
 
             BufferedReader infromClient = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
 
             DataOutputStream outToClient = new DataOutputStream(connSocket.getOutputStream());
 
             clientString = infromClient.readLine();
+            System.out.println(clientString);
             
             switch(clientString) {
                 case "1": 
-                    URL url = new URL("https://ieee.org");
-                    URL page = getPage(url);
+                    w = "https://www.ieee.org:443";
                     break;
                 case "2":
-                    URL url2 = new URL("https://3gpp.org");
-                    URL page2 = getPage(url2);
+                    w = "https://www.3gpp.org:443";
                     break;
                 
                 case "3":
-                    URL url3 = new URL("https://eecs.mit.edu");
-                    URL page3 = getPage(url3);
+                    w ="https://www.eecs.mit.edu:443";
                     break; 
 
             }
 
+            URL obj = new URL(w);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            System.out.println("Request sent to " + w + "...");
+            
+            String inputLine = "";
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            while ((inputLine = in.readLine()) != null)
+                System.out.println(inputLine);
 
-
+            outToClient.writeBytes(confirmation + w + "\n");
+                welcomeSocket.close();
         }
     }
-    
 }
